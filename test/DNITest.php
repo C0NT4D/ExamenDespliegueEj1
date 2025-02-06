8ac9d329fdb4d1abdfe7190093eace173b656f6d
IvanContador <ivcoso@floridauniversitaria.es>
2025-02-06 10:11:19 +0100

    Primer commit

---
+++ test/DNITest.php
@@ -0,0 +1,14 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\DNI;
+
+class DNITest extends TestCase {
+    public function testCalcularLetraDNI() {
+        $dni = new DNI(12345678);
+        $resultado = $dni->getDNIConLetra(); 
+
+        $this->assertEquals("12345678Z", $resultado);
+    }
+}
