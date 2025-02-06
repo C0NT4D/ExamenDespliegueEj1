08a761ea821bedf73758a77bf259348ddc573205
IvanContador <ivcoso@floridauniversitaria.es>
2025-02-06 10:28:12 +0100

    Quinto commit

--- tests/DNITest.php
+++ tests/DNITest.php
@@ -3,7 +3,7 @@ namespace Tests;
 
 use PHPUnit\Framework\TestCase;
 use App\DNI;
-//a
+//aa
 class DNITest extends TestCase {
     public function testCalcularLetraDNI() {
         $dni = new DNI(12345678);
